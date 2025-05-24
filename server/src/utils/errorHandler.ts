c2e73545ad9d94f1d5a30880b421f4aaf04aa972
Alok <mishraalok189381@gmail.com>
2025-05-24 13:03:48 +0530

    fixed the return type issue of error handler

--- server/src/utils/errorHandler.ts
+++ server/src/utils/errorHandler.ts
@@ -2,16 +2,18 @@ import { NextFunction, Request, Response } from "express";
 
 export const errorHandler = (err: any,req: Request,res: Response,next: NextFunction) => {
   if (err instanceof AppError) {
-    return res.status(err.statusCode).json({
+     res.status(err.statusCode).json({
       success: false,
       message: err.message,
     });
+    return;
   }
 
   res.status(500).json({
     success: false,
     message: err.message || "Internal Server Error",
   });
+
 };
 
 export class AppError extends Error {
